bd66196a884eb394da136f87a19a2c7bc90f0dbc
Kaf <53307144+iamkaf@users.noreply.github.com>
2025-06-13 19:08:28 -0300

    Update to ESLint 9 and add flat config

---
+++ apps/mc-pack-tool/eslint.config.js
@@ -0,0 +1,36 @@
+const js = require('@eslint/js');
+const { FlatCompat } = require('@eslint/eslintrc');
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+module.exports = [
+  {
+    ignores: [
+      'node_modules/*',
+      'dist/*',
+      'coverage/*',
+      '.vite/*',
+      '.out/*',
+      '.webpack/*',
+      '*.js',
+    ],
+  },
+  js.configs.recommended,
+  ...compat.config({
+    env: {
+      browser: true,
+      es6: true,
+      node: true,
+    },
+    extends: [
+      'plugin:@typescript-eslint/eslint-recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:import/recommended',
+      'plugin:import/electron',
+      'plugin:import/typescript',
+    ],
+    parser: '@typescript-eslint/parser',
+  }),
+];
