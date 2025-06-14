c0b527476e100774614d0d1acd4abdb9b73c2a05
Kaf <53307144+iamkaf@users.noreply.github.com>
2025-06-14 02:26:28 -0300

    chore: collapse workspace

--- src/renderer/components/Spinner.tsx
+++ src/renderer/components/Spinner.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import { TailSpin } from 'react-loader-spinner';
-
 export default function Spinner() {
   return (
     <div className="flex justify-center p-4" data-testid="spinner">
-      <TailSpin
-        height={48}
-        width={48}
-        color="hsl(var(--bc))"
-        ariaLabel="loading"
+      <span
+        className="loading loading-spinner text-primary"
+        aria-label="loading"
       />
     </div>
   );
